3ebbe94b9ad05e0da83817d8a5fe7f69a9d94cae
alexandr <terzisanya4774@gmail.com>
2023-05-15 15:19:20 +0300

    main

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let block = document.querySelector('.block');
+let widthX = window.innerWidth;
+let heightY = window.innerHeight;
+
+const randColor = () =>  {
+    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
+
+const randPositionX = () =>  {
+    return Math.floor(Math.random()*widthX) + 'px';
+}
+const randPositionY = () =>  {
+    return Math.floor(Math.random()*heightY) + 'px';
+}
+
+function getRandColorForBlock() {
+    return block.style.backgroundColor = `${randColor()}`;
+}
+
+function getRandPosition() {
+    return block.style.left = `${randPositionX()}`,
+    block.style.top = `${randPositionY()}`;
+}
+
+setInterval(getRandColorForBlock, 500);
+setInterval(getRandPosition, 1000);
